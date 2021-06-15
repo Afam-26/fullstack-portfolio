85c97e3208e205c5618b7b256f87a573d01b2721
Afamefuna Anekeh <afamefunaanekeh@Afamefunas-MBP.attlocal.net>
2021-06-15 17:33:25 -0500

    adding style to about me page

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -1,5 +1,6 @@
 import React from "react"
-// import './style.css'
+import Typical from "react-typical";
+import './style.css'
 
 function AboutMe(){
     return (
@@ -8,21 +9,51 @@ function AboutMe(){
                 <div className="card w-75 min-vh-50 aboutMeCardContainer justify-content-center align-items-center">
                     <div className="card-body my-2 aboutMeCard">
                       <h5 className="card-title">About Me</h5>
+                   
                       <p className="card-text overflow-auto"> 
                         I earned my undergrad in Public Administration in 2009, and I worked as a Sales Manager at a tire shop where I worked closely with upper management,
                         met sales goals and lead a small team of sales associates. There, is where I gained my leadership skills and became a strong organizer and coordinator.  
-   
+                        <br/>
+                        <br/>
 
                         After graduating from ACC with an Associate degree in LAN System Security and certificate in Network Admin, I joined Whole Foods Market as Sr. Service Desk Analyst.
                         There, is where I improved my leadership skills with the ability to motivate and influence others to deliver best results.
                         Proficiency at understanding, articulating and solving both complex and complicated problems  
-                    
-
-                        This is a group project for my UT bootcamp. https://afam-26.github.io/Cocktail-on-the-run/ 
-                        This website assist individuals who are interested in knowing how their favorite cocktails are made. 
-                        In addition, they can search for their favorite cocktails location near them and they will be presented with list of locations. 
+                        <br/>
+                        <br/>            
                     </p>
+                    <p className="my-6 text-blueGray-100">
+              My skills include:{" "}
+              <Typical
+                className="font-mono"
+                steps={[
+                  "JavaScript",
+                  1500,
+                  "React.js",
+                  1200,
+                  "MongoDB",
+                  1200,
+                  "Node.js",
+                  1000,
+                  "Express.js",
+                  900,
+                  "MySQL",
+                  800,
+                  "HTML",
+                  900,
+                  "CSS",
+                  900,
+                  "jQuery",
+                  1000,
+                  "Bootstrap",
+                  1000,
+                ]}
+                loop={Infinity}
+                wrapper="p"
+              />
+            </p>
                     </div>
+                    
                 </div>
             </section>
         </div>
